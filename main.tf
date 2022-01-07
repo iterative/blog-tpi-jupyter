24ef15451d587596dc4689df47979383a2b90640
Casper da Costa-Luis <casper.dcl@physics.org>
2022-01-07 20:01:52 +0000

    more tidy

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
 terraform {
-  required_providers {
-    iterative = { source = "iterative/iterative" }
-  }
+  required_providers { iterative = { source = "iterative/iterative" } }
 }
 provider "iterative" {}
-resource "iterative_task" "casper_jupyter" {
-  name    = "casper_jupyter"
+resource "iterative_task" "jupyter_server" {
+  name    = "casper"
   cloud   = "aws"
   machine = "g4dn.xlarge"
   image   = "user@*:x86_64:Deep Learning AMI GPU TensorFlow 2.7.0 (Ubuntu 20.04) 20211208"
@@ -14,31 +12,36 @@ resource "iterative_task" "casper_jupyter" {
   directory   = "${path.root}/shared"
   script      = <<-END
     #!/bin/bash
-    set -e
+    set -euo pipefail
     pip3 install notebook tensorflow tensorboard cuvec
     curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
-    apt update -qq && apt install -yqq nodejs
+    apt install -yqq nodejs
 
-    tmpdir="$(mktemp -d)"
-    cp tunnel.js "$tmpdir"/
-    pushd "$tmpdir"
+    pushd "$(mktemp -d --suffix ngrok-tunnel)"
     npm i ngrok
-    if [[ -n "$NGROK_TOKEN" ]]; then npx ngrok authtoken "$NGROK_TOKEN"; fi
-    node tunnel.js &
+    npx ngrok authtoken "$NGROK_TOKEN"
+    (node <<-EOF
+    const fs = require('fs');
+    const ngrok = require('ngrok');
+    (async function() {
+      const jupyter = await ngrok.connect(8888);
+      const tensorboard = await ngrok.connect(6006);
+      fs.writeFileSync("log.md", \`Jupyter Notebook: \$${jupyter}\nTensorboard: \$${tensorboard}\n\`);
+    })();
+    EOF
+    ) &
     while [[ ! -f log.md ]]; do sleep 1; done
     cat log.md
     popd
-    rm -rf "$tmpdir"
 
     env -u AWS_ACCESS_KEY_ID -u AWS_SECRET_ACCESS_KEY -u REPO_TOKEN PASSWORD="$JUPYTER_PASSWORD" tensorboard --logdir . --host 0.0.0.0 --port 6006 &
 
-    JUPYTER_SHA="$(python3 -c "from IPython.lib import passwd; print(passwd('$JUPYTER_PASSWORD'), end='')")"
     mkdir -p ~/.jupyter
     echo '{
       "NotebookApp": {
         "allow_root": true, "ip": "0.0.0.0", "open_browser": false,
-        "password": "'$JUPYTER_SHA'", "password_required": true,
-        "port": 8888, "port_retries": 0,
+        "password": "'$(python3 -c "from IPython.lib import passwd; print(passwd('$JUPYTER_PASSWORD'), end='')")'",
+        "password_required": true, "port": 8888, "port_retries": 0,
         "shutdown_no_activity_timeout": 86400
       }
     }' > ~/.jupyter/jupyter_notebook_config.json
